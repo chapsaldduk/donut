45d9403e7042ac94758f8a0751c26d5b78533096
chapsaldduck <koala031532@gmail.com>
2022-11-24 16:49:30 +0900

    donut

--- donut.c
+++ donut.c
@@ -18,11 +18,11 @@ int main(){
                 sinj = sin(j),
                 cosA = cos(A),
                 cosj2 = cosj + 2,
-                mess = 1/(sini*cosj2*sinA+sinj*consA+5),
+                mess = 1/(sini*cosj2*sinA+sinj*cosA+5),
                 cosi = cos(i),
                 cosB = cos(B),
                 sinB = sin(B),
-                t = sini*cosj2*consA-sinj*sinA;
+                t = sini*cosj2*cosA-sinj*sinA;
                 
                 int x = 40+30*mess*(cosi*cosj2*cosB - t*sinB),
                 y = 12+15*mess*(cosi*cosj2*sinB + t*cosB),
@@ -36,7 +36,7 @@ int main(){
             }
         }
         printf("\x1b[d");
-        for(int k = 0, k<1761; k++){
+        for(int k = 0; k<1761; k++){
             putchar(k%80?b[k]:10);
         }
         A += 0.04;
